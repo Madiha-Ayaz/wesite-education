542fc862a621c37608ffe5ba2602680a75ca9e97
Madiha-Ayaz <161247604+Madiha-Ayaz@users.noreply.github.com>
2024-10-20 20:24:26 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import style from "./contact.module.css";
+
+import HeroContact from "../components/heroContact/page";
+
+const page = () => {
+  return (
+    <section id="contact" className="contact-section">
+
+<HeroContact/>
+</section>
+  )
+}
+
+export default page
